c01abb1d74a86d88f67ff0c1b283b479499c5340
Paolo Miguel Flores <paolo.flores@GXCHLAP00695.local>
2025-07-01 15:39:58 +0800

    Final Submission, Lab 2: Membership Calculator

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,46 @@ import java.util.*;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        float Price = 0;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter membership status (Regular, VIP, Premium): ");
+        String membershipStatus = scanner.next();
+        System.out.print("Enter age: ");
+        int memberAge = scanner.nextInt();
 
+        if (Objects.equals(membershipStatus, "Regular")){
+            if (memberAge < 18){
+                System.out.printf("Price: $%.2f", Price + 50.00);
+            }
+            else if (memberAge <= 64){
+                System.out.printf("Price: $%.2f", Price + 100.00);
+            }
+            else {
+                System.out.printf("Price: $%.2f", Price + 75.00);
+            }
+        }
+        else if (Objects.equals(membershipStatus, "VIP")){
+            if (memberAge < 18){
+                System.out.printf("Price: $%.2f", Price + 75.00);
+            }
+            else if (memberAge <= 64){
+                System.out.printf("Price: $%.2f", Price + 150.00);
+            }
+            else {
+                System.out.printf("Price: $%.2f", Price + 112.50);
+            }
+        }
+        else if (Objects.equals(membershipStatus, "Premium")){
+            if (memberAge < 18){
+                System.out.printf("Price: $%.2f", Price + 100.00);
+            }
+            else if (memberAge <= 64){
+                System.out.printf("Price: $%.2f", Price + 200.00);
+            }
+            else {
+                System.out.printf("Price: $%.2f", Price + 150.00);
+            }
+        }
+        else System.out.println("Invalid Membership Input");
     }
 }
